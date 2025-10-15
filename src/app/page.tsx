d6b5a350f135ef5aeeda5d1c808f10062a45a58b
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-10-15 16:59:05 -0300

    base ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,92 @@
-import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { ButtonGroup } from '@/components/ui/button-group';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
+
+import { ArrowRight, Menu, Trash } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className='mx-auto max-w-7xl md:p-12 min-h-[26rem] h-screen flex items-center justify-center'>
+      <Card className='rounded-none shadow-none border-0 md:rounded-2xl md:shadow-md w-full max-w-lg h-full'>
+        <CardHeader>
+          <CardDescription className='grid grid-cols-[auto_1fr] gap-y-2 gap-x-3'>
+            <Button>
+              <Menu />
+            </Button>
+            <Button className='w-full'>
+              Adicionar <ArrowRight />
+            </Button>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='bg-red-50 rounded-md p-4 h-full'>
+          <Accordion type='multiple'>
+            <AccordionItem value='item-1'>
+              <AccordionTrigger className='capitalize pt-0'>
+                Is it accessible?
+              </AccordionTrigger>
+              <AccordionContent>
+                <ul className='space-y-5'>
+                  <li className='flex items-center gap-3 justify-between'>
+                    <div className='flex items-center gap-1.5'>
+                      <Checkbox id='terms-1' />
+                      <Label className='capitalize' htmlFor='terms-1'>
+                        teste - 1
+                      </Label>
+                    </div>
+                    <Button className='hidden' size={'sm'}>
+                      <Trash />
+                    </Button>
+                  </li>
+                  <li className='flex items-center gap-3 justify-between'>
+                    <div className='flex items-center gap-1.5'>
+                      <Checkbox id='terms-2' />
+                      <Label className='capitalize' htmlFor='terms-2'>
+                        teste - 2
+                      </Label>
+                    </div>
+                    <Button className='hidden' size={'sm'}>
+                      <Trash />
+                    </Button>
+                  </li>
+                  <li className='flex items-center gap-3 justify-between'>
+                    <div className='flex items-center gap-1.5'>
+                      <Checkbox id='terms-3' />
+                      <Label className='capitalize' htmlFor='terms-3'>
+                        teste - 3
+                      </Label>
+                    </div>
+                    <Button className='hidden' size={'sm'}>
+                      <Trash />
+                    </Button>
+                  </li>
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </CardContent>
+        <CardFooter className='flex justify-end'>
+          <Button variant='link' size='sm'>
+            Selecionar Todos
+          </Button>
+          <Button variant='link' size='sm'>
+            Limpar Seleção
+          </Button>
+        </CardFooter>
+      </Card>
+    </main>
   );
 }
