8839770d921464002c6d2c03953ad86d94014c27
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-10-15 20:57:33 -0300

    add website base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   trailingSlash: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
